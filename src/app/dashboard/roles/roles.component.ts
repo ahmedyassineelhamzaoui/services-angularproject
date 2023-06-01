94052fff446e384c4cf7031fc5bbf329fa850cf6
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-06-01 15:53:32 +0100

    add neccessary methods to fetch all roles

--- src/app/dashboard/roles/roles.component.ts
+++ src/app/dashboard/roles/roles.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { RoleService } from 'src/app/role.service';
+import { TokenService } from '../../shared/token.service';
+import { Role } from 'src/app/role';
 
 @Component({
   selector: 'app-roles',
@@ -6,5 +11,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./roles.component.css']
 })
 export class RolesComponent {
-
+   
+    isLoading = true
+    Roles !: Role[];
+    allRolesp : number = 0;
+    pagination  : number = 1;
+ 
+    deleteIndex : number = -1 ;
+    roleIndex   : number = -1 ;
+    editIndex   : number = -1 ;
+    constructor
+              (public formBuilder : FormBuilder,
+                public router : Router,
+                public ngZone : NgZone,
+                public token : TokenService,
+                public roleService : RoleService
+              )
+    {
+      this.fetchroles();
+    } 
+    getRolePermissionNames(role: any): string {
+      return role.permissions.map((permission: any) => permission.name);
+    }
+  
+    fetchroles(){
+          if(this.token.isLoggedIn()){
+            this.roleService.getAllRoles(this.pagination).subscribe((data: any) => {
+              if(data.error){
+                this.ngZone.run(()=> this.router.navigateByUrl('/home'))
+              }
+              if(data.permissions){
+                this.ngZone.run(()=> this.router.navigateByUrl('/permissions'))
+              }
+              this.Roles = data.roles;
+              console.log(this.Roles)
+              this.isLoading = false;
+            });
+          }else{
+            this.ngZone.run(()=> this.router.navigateByUrl('/home'))
+          }
+    }
+    renderPage(event: number) {
+      this.pagination = event;
+      this.fetchroles();
+    }
 }
---
+++ src/app/role.service.ts
@@ -0,0 +1,39 @@
+import { Injectable,NgZone } from '@angular/core';
+import { HttpClient,HttpHeaders,HttpErrorResponse } from '@angular/common/http';
+import { TokenService } from './shared/token.service';
+import { Role } from './role';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+
+
+  httpHeaders = new HttpHeaders().set('content-type','application/json');
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(
+    private http:HttpClient,
+    public router:Router,
+    public ngZone:NgZone,
+    public token: TokenService,
+  ) { }
+
+  
+  getAllRoles(page: number): Observable<any>{
+     return this.http.get<any>(this.apiUrl + '/roles?page='+page).pipe(catchError(this.handlError));
+  }
+
+
+  handlError(error:HttpErrorResponse){
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = error.error.message;
+    }else{
+      errorMessage = `Èrror Code : ${error.status}\n Message: ${error.message}`
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
